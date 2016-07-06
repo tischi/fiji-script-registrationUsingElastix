2edd1d049b37d897618f5d48f7ac2e6c4bb1bb53
Christian "Tischi" Tischer <tischitischer@gmail.com>
2016-07-06 12:22:29 +0200

    added files

--- fiji--elastix.py
+++ fiji--elastix.py
@@ -27,8 +27,12 @@ def convert_to_mhd(input_file, output_folder):
   IJ.run(imp, "MHD/MHA ...", "save="+output_file);
   return output_file
 
+# the conversion to unsigned int is ugly!
 def convert_result(original_filename, output_folder):
   imp = IJ.openImage(os.path.join(output_folder,"result.0.mhd"));
+  IJ.run(imp, "32-bit", "");
+  IJ.setMinAndMax(0, 65535);
+  IJ.run(imp, "16-bit", "");
   output_file = os.path.join(output_folder,original_filename+"--transformed.tif")
   IJ.saveAs(imp, "Tiff", output_file);
   return output_file
@@ -39,7 +43,7 @@ def convert_result(original_filename, output_folder):
 # - input folder
 input_folder = "C:\\Users\\tischer\\Desktop"
 # get file list
-file_list = ['t10.tif', 't09.tif']
+file_list = ['t09.tif', 't10.tif']
 # - output folder
 output_folder = "C:\\Users\\tischer\\Desktop"
 # - channel for computing registration
@@ -47,6 +51,7 @@ output_folder = "C:\\Users\\tischer\\Desktop"
 #   - against which file (fixed file, running file)
 #   - which method (rigid, euler, affine)
 parameter_file = "Z:/0000--Tischi--Practical_ImageJ/data-all/registration/elastix_example_v4.8/exampleinput/parameters_Rigid.txt"
+parameter_file = "Z:/0000--Tischi--Practical_ImageJ/data-all/registration/elastix_example_v4.8/exampleinput/parameters_Affine.txt"
 # path to elastix
 elastix = 'C:/Program Files/elastix_v4.8/elastix' 
 
@@ -58,6 +63,7 @@ elastix = 'C:/Program Files/elastix_v4.8/elastix'
 # convert fixed and moving file to .mhd (use output folder as temp folder for the mhd files)
 fixed_file = convert_to_mhd(os.path.join(input_folder,file_list[0]), output_folder)
 moving_file = convert_to_mhd(os.path.join(input_folder,file_list[1]), output_folder)
+
 
 # run registration
 output = cmd([elastix, '-f', fixed_file,'-m', moving_file, '-out', output_folder, '-p', parameter_file])
