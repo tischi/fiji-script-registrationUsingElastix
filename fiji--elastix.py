219a3bd0291795c47888ec27ed6cd31ecfd14cfc
Christian "Tischi" Tischer <tischitischer@gmail.com>
2016-07-06 15:27:57 +0200

    works with input folders now

--- fiji--elastix.py
+++ fiji--elastix.py
@@ -3,13 +3,14 @@
 #
 # author: tischitischer@gmail.com
 #
-# input format: 
-#  -
+# input: 
+# 
+# computation:
 #
+# output:
 
 import os
 from ij import IJ
-import subprocess
 from subprocess import Popen, PIPE
 
 #
@@ -21,58 +22,85 @@ def cmd(args):
   output, err = p.communicate()
   return(output)
 
-def convert_to_mhd(input_file, output_folder):
-  imp = IJ.openImage(input_file);
-  output_file = os.path.join(output_folder,input_file+".mhd")
+def convert_to_mhd(input_file, mhd_file_name, output_folder):
+  #print("opening "+input_file) 
+  imp = IJ.openImage(input_file)
+  output_file = os.path.join(output_folder, mhd_file_name)
   IJ.run(imp, "MHD/MHA ...", "save="+output_file);
+  #print("saving "+output_file)
   return output_file
 
-# the conversion to unsigned int is ugly!
-def convert_result(original_filename, output_folder):
-  imp = IJ.openImage(os.path.join(output_folder,"result.0.mhd"));
+# - the conversion to unsigned int is ugly!
+# - weird behavior of IJ.openImage: it shows the image but does not put anything into the imp
+#   - report to Wayne
+def convert_result(original_filename, output_folder, save_as_max):
+  IJ.openImage(os.path.join(output_folder,"result.0.mhd"))
+  imp = IJ.getImage()
   IJ.run(imp, "32-bit", "");
   IJ.setMinAndMax(0, 65535);
   IJ.run(imp, "16-bit", "");
   output_file = os.path.join(output_folder,original_filename+"--transformed.tif")
-  IJ.saveAs(imp, "Tiff", output_file);
+  IJ.saveAs(imp, "Tiff", output_file)
+  # optional to save maximum projections
+  if(save_as_max):
+    if(imp.getStackSize()>1):
+      IJ.run(imp, "Z Project...", "projection=[Max Intensity]")
+      IJ.saveAs(IJ.getImage(), "Tiff", output_file+"--z-max.tif")
   return output_file
 
 #
 # GET PARAMETERS
 #
+
 # - input folder
-input_folder = "C:\\Users\\tischer\\Desktop"
-# get file list
-file_list = ['t09.tif', 't10.tif']
-# - output folder
-output_folder = "C:\\Users\\tischer\\Desktop"
-# - channel for computing registration
-# - registration strategy
-#   - against which file (fixed file, running file)
-#   - which method (rigid, euler, affine)
-parameter_file = "Z:/0000--Tischi--Practical_ImageJ/data-all/registration/elastix_example_v4.8/exampleinput/parameters_Rigid.txt"
-parameter_file = "Z:/0000--Tischi--Practical_ImageJ/data-all/registration/elastix_example_v4.8/exampleinput/parameters_Affine.txt"
+input_folder = "C:\\Users\\tischer\\Desktop\\3D-files"
+
+# file list
+# file_list = ['t09.tif', 't10.tif']
+
+file_list = []
+for file_name in os.listdir(input_folder):
+  if file_name.endswith(".tif"):
+    file_list.append(file_name)
+
+# output folder
+output_folder = input_folder+"--fiji"
+if not os.path.isdir(output_folder):
+  os.mkdir(output_folder)
+
+# registration method
+parameter_file = "C:\\Users\\tischer\\Desktop\\parameters_Affine.txt"
+
+# reference image
+i_ref = int(len(file_list)/2)
+
 # path to elastix
 elastix = 'C:/Program Files/elastix_v4.8/elastix' 
+
+# options
+save_result_also_as_max_projection = True
 
 #
 # COMPUTE
 #
-# loop through files
 
-# convert fixed and moving file to .mhd (use output folder as temp folder for the mhd files)
-fixed_file = convert_to_mhd(os.path.join(input_folder,file_list[0]), output_folder)
-moving_file = convert_to_mhd(os.path.join(input_folder,file_list[1]), output_folder)
+# convert fixed (reference) file to .mhd 
+fixed_file = convert_to_mhd(os.path.join(input_folder, file_list[i_ref]), "fixed.mhd", output_folder)
 
+for f in file_list:
+  print("matching: "+str(f)+" to "+file_list[i_ref])
+  moving_file = convert_to_mhd(os.path.join(input_folder, f), "moving.mhd", output_folder)
+  
+  # run registration
+  output = cmd([elastix, '-f', fixed_file,'-m', moving_file, '-out', output_folder, '-p', parameter_file])
 
-# run registration
-output = cmd([elastix, '-f', fixed_file,'-m', moving_file, '-out', output_folder, '-p', parameter_file])
-print(output)
+  # convert transformed moving file to .tif and give it the name of the input file
+  convert_result(f, output_folder, save_result_also_as_max_projection)
+  
+  # clean up
+  IJ.run("Close All", "");
 
-# convert transformed moving file to .tif
-convert_result(file_list[1], output_folder)
-
-
+print("Done!")
 # optional:
 # - save x,y,z maximum projections of the raw and registered data on the fly and show them in the end as movies
 
