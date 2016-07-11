59f507481b4cd55a6d2b4a461bfed2e91c585ba4
Christian "Tischi" Tischer <tischitischer@gmail.com>
2016-07-11 09:30:06 +0200

    update

--- fiji--elastix.py
+++ fiji--elastix.py
@@ -23,13 +23,13 @@
 #
 # Todo:
 # - use masks
-# - use last trafo as input for next
+# - use last trafo as input for next: -t0 TransformParameters.0.txt
 # - when it is 8-bit input save as 8-bit input (check the unsigned issue)
 # - option to find trafo in downsampled and apply to full-size
 # - tmp storage on VM that is automatically deleted upon log-off?
 
 
-import os, time
+import os, time, shutil, sys
 from ij import IJ
 from subprocess import Popen, PIPE
 
@@ -42,7 +42,7 @@ def cmd(args):
   output, err = p.communicate()
   return(output)
      
-def convert_to_mhd(input_file, mhd_file_name, output_folder):
+def convert_for_elastix(input_file, mhd_file_name, output_folder):
   print("    reading tif")
   start_time = time.time()
   imp = IJ.openImage(input_file)
@@ -57,10 +57,10 @@ def convert_to_mhd(input_file, mhd_file_name, output_folder):
 # - the conversion to unsigned int is ugly!
 # - weird behavior of IJ.openImage: it shows the image but does not put anything into the imp
 #   - report to Wayne
-def convert_result(original_filename, output_folder, save_as_max):
-  print("    reading mhd")
+def convert_from_elastix(original_filename, output_folder, save_as_max):
+  print("    reading mha")
   start_time = time.time()
-  IJ.openImage(os.path.join(output_folder,"result.0.mhd"))
+  IJ.openImage(os.path.join(output_folder,"result.0.mha"))
   print("      time spent: "+str(round(time.time()-start_time,3)))
   imp = IJ.getImage()
   IJ.run(imp, "32-bit", "");
@@ -75,8 +75,10 @@ def convert_result(original_filename, output_folder, save_as_max):
   if(save_as_max):
     if(imp.getStackSize()>1):
       print("    make and save z-max")
+      start_time = time.time()
       IJ.run(imp, "Z Project...", "projection=[Max Intensity]")
       IJ.saveAs(IJ.getImage(), "Tiff", output_file+"--z-max.tif")
+      print("      time spent: "+str(round(time.time()-start_time,3)))
   return output_file
 
 #
@@ -84,8 +86,22 @@ def convert_result(original_filename, output_folder, save_as_max):
 #
 
 # - input folder
-input_folder = "X:\\Henning\\Tischi_Reg\\MIP_subset_down-sized"
+input_folder = "X:\\Henning\\Tischi_Reg\\MIP"
 #input_folder = "C:\\Users\\tischer\\Desktop\\test"
+
+# registration method
+parameter_file = "C:\\Users\\tischer\\Desktop\\parameters_Affine.txt"
+
+# registration strategy
+trafo = "running"
+#trafo = "fixed"
+
+
+# path to elastix
+elastix = 'C:/Program Files/elastix_v4.8/elastix' 
+
+# options
+save_as_max = True
 
 # file list
 # file_list = ['t09.tif', 't10.tif']
@@ -95,6 +111,9 @@ for file_name in os.listdir(input_folder):
   if file_name.endswith(".tif"):
     file_list.append(file_name)
 
+# reference image
+i_ref = int(round(len(file_list)/2))
+
 # output folder
 output_folder = input_folder+"--fiji"
 if not os.path.isdir(output_folder):
@@ -102,43 +121,76 @@ if not os.path.isdir(output_folder):
 
 #print(file_list)
 
-# registration method
-parameter_file = "C:\\Users\\tischer\\Desktop\\parameters_Affine.txt"
-
-# reference image
-i_ref = int(len(file_list)/2)
-
-# path to elastix
-elastix = 'C:/Program Files/elastix_v4.8/elastix' 
-
-# options
-save_as_max = True
 
 #
 # COMPUTE
 #
 
-# convert fixed (reference) file to .mhd 
-print("converting reference file")
-fixed_file = convert_to_mhd(os.path.join(input_folder, file_list[i_ref]), "fixed.mhd", output_folder)
 
-for f in file_list:
-  print("matching: "+str(f)+" to "+file_list[i_ref])
-  print("  converting .tif to .mhd")
-  moving_file = convert_to_mhd(os.path.join(input_folder, f), "moving.mhd", output_folder)
+i_ref = 1
+
+if(trafo=="running"):
   
-  # run registration
-  print("  running elastix")
-  start_time = time.time()
-  output = cmd([elastix, '-f', fixed_file,'-m', moving_file, '-out', output_folder, '-p', parameter_file])
-  print("      time spent: "+str(round(time.time()-start_time,3)))
-  #print(output)
-  # convert transformed moving file to .tif and give it the name of the input file
-  print("  converting .mhd to .tif")
-  convert_result(f, output_folder, save_as_max)
+  print("converting reference file: " + file_list[i_ref])
+  convert_for_elastix(os.path.join(input_folder, file_list[i_ref]), "previous.mha", output_folder)
+  previous_result_file = os.path.join(output_folder, "previous.mha")
+    
+  for k, i in enumerate(range(i_ref+3,i_ref+10,3)):
+    print("matching: "+file_list[i])
+    
+    print("  converting .tif to .mha")
+    next_file = convert_for_elastix(os.path.join(input_folder, file_list[i]), "moving.mha", output_folder)
+    
+    # run registration
+    print("  running elastix")
+    start_time = time.time()
+    if k==0:
+      output = cmd([elastix, '-f', previous_result_file, '-m', next_file, '-out', output_folder, '-p', parameter_file])
+    else:
+      print("    using previous transformation for initialisation")
+      os.remove(os.path.join(output_folder, "previous.mha"))
+      time.sleep(1)
+      os.rename(os.path.join(output_folder, "result.0.mha"), os.path.join(output_folder, "previous.mha"))
+      sys.exit(1)
+      previous_transformation = os.path.join(output_folder, "TransformParameters.0.txt")
+      output = cmd([elastix, '-f', previous_result_file, '-m', next_file, '-out', output_folder, '-p', parameter_file, '-t0', previous_transformation])
+    
+    print("      time spent: "+str(round(time.time()-start_time,3)))
+    #print(output)
+    
+    # convert transformed moving file to .tif and give it the name of the input file
+    #print("  converting .mha to .tif")
+    
+    # replace this by a simple file copy!!
+    #convert_from_elastix(file_list[i], output_folder, save_as_max)
+
+    
+    # clean up
+    IJ.run("Close All", "");
+
+
+if(trafo=="fixed"):
   
-  # clean up
-  IJ.run("Close All", "");
+  print("converting reference file")
+  fixed_file = convert_to_mhd(os.path.join(input_folder, file_list[i_ref]), "fixed.mhd", output_folder)
+  
+  for f in file_list:
+    print("matching: "+str(f)+" to "+file_list[i_ref])
+    print("  converting .tif to .mhd")
+    moving_file = convert_to_mhd(os.path.join(input_folder, f), "moving.mhd", output_folder)
+    
+    # run registration
+    print("  running elastix")
+    start_time = time.time()
+    output = cmd([elastix, '-f', fixed_file,'-m', moving_file, '-out', output_folder, '-p', parameter_file])
+    print("      time spent: "+str(round(time.time()-start_time,3)))
+    #print(output)
+    # convert transformed moving file to .tif and give it the name of the input file
+    print("  converting .mhd to .tif")
+    convert_result(f, output_folder, save_as_max)
+    ddd
+    # clean up
+    IJ.run("Close All", "");
 
 print("Done!")
 
